9129e3a4f4052f3a0ee133a24fcd317199e567f5
firdausk2020 <halimc17@yahoo.com>
2025-10-16 14:59:54 +0700

    Revert "apply harga pasar palm product"
    This reverts commit 3cdf9e37ebf2a8bf28943005b55474b1a066f937.

--- pmn_hargaPasar.php
+++ pmn_hargaPasar.php
@@ -49,97 +49,84 @@ $optStatus="<option value='Best Bidder'>Best Bidder</option>";
 $optStatus.="<option value='Price Idea'>Price Idea</option>";
 $optStatus.="<option value='Traded'>Traded</option>";
 
-echo"<div class='card' style='max-width:400px;'>
-     <div class='card-header'><strong>".$_SESSION['lang']['hargapasar']."</strong></div>
-     <div class='card-body'>
-	 <table class='table table-sm table-borderless'>
+echo"<fieldset style=width:250px>
+     <legend>".$_SESSION['lang']['hargapasar']."</legend>
+	 <table>
 	 <tr>
-	   <td style='width:140px;'>".$_SESSION['lang']['tanggal']."</td>
-	   <td><input type=text class='form-control form-control-sm' id=tglHarga onmousemove=setCalendar(this.id) onkeypress='return false;' maxlength=10 /></td>
+	   <td>".$_SESSION['lang']['tanggal']."</td>
+	   <td><input type=text class=myinputtext id=tglHarga onmousemove=setCalendar(this.id) onkeypress=return false;  size=10 maxlength=10 style=\"width:150px;\" /></td>
 	 </tr>
 	 <tr>
 	   <td>".$_SESSION['lang']['komoditi']."</td>
-	   <td><select id=kdBarang class='form-select form-select-sm'>".$optBrg."</select></td>
+	   <td><select id=kdBarang style=\"width:150px;\">".$optBrg."</select></td>
 	 </tr>
 	 <tr>
 	   <td>".$_SESSION['lang']['satuan']."</td>
-	   <td><select id=satuan class='form-select form-select-sm'>".$optKodeSat."</select></td>
-	 </tr>
+	   <td><select id=satuan style=\"width:150px;\">".$optKodeSat."</select></td>
+	 </tr>	
 	 <tr>
 	   <td>".$_SESSION['lang']['pasar']."</td>
-	   <td><select id=idPasar class='form-select form-select-sm'>".$optPasar."</select></td>
-	 </tr>
+	   <td><select id=idPasar style=\"width:150px;\">".$optPasar."</select></td>
+	 </tr>	 
 	  <tr>
 	   <td>".$_SESSION['lang']['matauang']."</td>
-	   <td><select id=idMatauang class='form-select form-select-sm'>".$optKode."</select></td>
-	 </tr>
+	   <td><select id=idMatauang style=\"width:150px;\">".$optKode."</select></td>
+	 </tr> 
           <tr>
 	   <td>".$_SESSION['lang']['harga']."</td>
-	   <td><input type=text class='form-control form-control-sm' id=hrgPasar onkeypress=\"return angka_doang(event);\" /> </td>
-	 </tr>
-
+	   <td><input type=text class=myinputtextnumber id=hrgPasar onkeypress=\"return angka_doang(event);\" style=\"width:150px;\"  /> </td>
+	 </tr>	
+         
          <tr>
 	   <td>".$_SESSION['lang']['status']." ".$_SESSION['lang']['harga']."</td>
-	   <td><select id=status class='form-select form-select-sm'>".$optStatus."</select></td>
-	 </tr>
+	   <td><select id=status style=\"width:150px;\">".$optStatus."</select></td>
+	 </tr> 
          <tr>
 	   <td>FFA</td>
-	   <td><input type=text class='form-control form-control-sm' id=ffa onkeypress=\"return angka_doang(event);\" /> </td>
-	 </tr>
-
+	   <td><input type=text class=myinputtextnumber id=ffa onkeypress=\"return angka_doang(event);\" style=\"width:150px;\"  /> </td>
+	 </tr>	
+         
         <tr>
 	   <td>M & I</td>
-	   <td><input type=text class='form-control form-control-sm' id=mni onkeypress=\"return angka_doang(event);\" /> </td>
-	 </tr>
+	   <td><input type=text class=myinputtextnumber id=mni onkeypress=\"return angka_doang(event);\" style=\"width:150px;\"  /> </td>
+	 </tr>	
 
 	 </table>
 	 <input type=hidden value=insert id=proses>
-	 <div class='mt-2'>
-	 <button class='btn btn-primary btn-sm me-1' onclick=saveFranco('pmn_slave_hargapasar','".$arr."')><i class='bi bi-save'></i> ".$_SESSION['lang']['save']."</button>
-	 <button class='btn btn-secondary btn-sm' onclick=cancelIsi()><i class='bi bi-x-circle'></i> ".$_SESSION['lang']['cancel']."</button>
-	 </div>
-     </div>
-     </div><input type='hidden' id=idFranco name=idFranco />";
+	 <button class=mybutton onclick=saveFranco('pmn_slave_hargapasar','".$arr."')>".$_SESSION['lang']['save']."</button>
+	 <button class=mybutton onclick=cancelIsi()>".$_SESSION['lang']['cancel']."</button>
+     </fieldset><input type='hidden' id=idFranco name=idFranco />";
 CLOSE_BOX();
 OPEN_BOX();
 
-echo"<div class='card'>
-     <div class='card-header'><strong>".$_SESSION['lang']['list']."</strong></div>
-     <div class='card-body'>";
-echo"<div class='row g-2 mb-3 align-items-center'>
-     <div class='col-auto'><label class='col-form-label col-form-label-sm'>".$_SESSION['lang']['tanggal'].":</label></div>
-     <div class='col-auto'><input type=text class='form-control form-control-sm' id=tglCri onmousemove=setCalendar(this.id) onkeypress='return false;' maxlength=10 style='width:120px;' /></div>
-     <div class='col-auto'><label class='col-form-label col-form-label-sm'>".$_SESSION['lang']['komoditi'].":</label></div>
-     <div class='col-auto'><select id=kdBrgCari class='form-select form-select-sm' style='width:180px;'>".$optBrg."</select></div>
-     <div class='col-auto'><label class='col-form-label col-form-label-sm'>".$_SESSION['lang']['pasar'].":</label></div>
-     <div class='col-auto'><select id=idPsrCari class='form-select form-select-sm' style='width:150px;'>".$optPasar."</select></div>
-     <div class='col-auto'><button class='btn btn-primary btn-sm' onclick=cariTransaksi()><i class='bi bi-search'></i> ".$_SESSION['lang']['find']."</button></div>
-     </div>";
-echo"<div class='table-responsive'>
-    <table class='table table-striped table-hover table-sm'>
-     <thead class='table-dark'>
-	  <tr>
-	   <th>No</th>
-	   <th>".$_SESSION['lang']['tanggal']."</th>
-	   <th>".$_SESSION['lang']['komoditi']."</th>
-	   <th>".$_SESSION['lang']['satuan']."</th>
-	   <th>".$_SESSION['lang']['pasar']."</th>
-	   <th>".$_SESSION['lang']['matauang']."</th>
-           <th>".$_SESSION['lang']['harga']."</th>
-           <th>".$_SESSION['lang']['status']." ".$_SESSION['lang']['harga']."</th>
-               <th>FFA</th>
-               <th>M & I</th>
-	   <th>Action</th>
+echo"<fieldset  style=width:650px><legend>".$_SESSION['lang']['list']."</legend>";
+echo"<table cellpadding=1 cellspacing=1 border=0><tr><td>".$_SESSION['lang']['tanggal']." : <input type=text class=myinputtext id=tglCri onmousemove=setCalendar(this.id) onkeypress=return false;  size=10 maxlength=10  />";
+echo"&nbsp;".$_SESSION['lang']['komoditi']." : <select id=kdBrgCari style=\"width:150px;\">".$optBrg."</select>";
+echo"&nbsp;".$_SESSION['lang']['pasar']." : <select id=idPsrCari style=\"width:150px;\">".$optPasar."</select><button class=mybutton onclick=cariTransaksi()>".$_SESSION['lang']['find']."</button></td></tr></table>";
+echo"
+    <table class=sortable cellspacing=1 border=0>
+     <thead>
+	  <tr class=rowheader>
+	   <td>No</td>
+	   <td>".$_SESSION['lang']['tanggal']."</td>
+	   <td>".$_SESSION['lang']['komoditi']."</td>
+	   <td>".$_SESSION['lang']['satuan']."</td>
+	   <td>".$_SESSION['lang']['pasar']."</td>
+	   <td>".$_SESSION['lang']['matauang']."</td>
+           <td>".$_SESSION['lang']['harga']."</td>
+           <td>".$_SESSION['lang']['status']." ".$_SESSION['lang']['harga']."</td>  
+               <td>FFA</td>
+               <td>M & I</td>
+	   <td>Action</td>
 	  </tr>
 	 </thead>
 	 <tbody id=container>";
 	 echo"<script>loadData()</script>";
 
 echo"</tbody>
-     </table>
-     </div>
-     </div>
-     </div>";
+     <tfoot>
+	 </tfoot>
+	 </table></fieldset>";
 CLOSE_BOX();
 echo close_body();
 ?>
